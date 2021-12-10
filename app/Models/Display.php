a0edb1614c4a569a98d3570db6a38d1bd1268dad
Emanuel Dos Santos <emanueldossantoset5@gmail.com>
2021-12-10 13:16:13 -0300

    Subo proyecto inicial

---
+++ app/Models/Display.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Display extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'company_id',
+        'latitude',
+        'longitude',
+        'type',
+        'price'
+    ];
+}
---
+++ database/migrations/2021_12_10_160608_create_display_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisplayTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('display', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('company_id');
+            $table->decimal('latitude');
+            $table->decimal('longitude');
+            $table->enum('type',['indoor','outdoor']);
+            $table->decimal('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('display');
+    }
+}
