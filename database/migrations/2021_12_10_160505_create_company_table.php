e964538c69cfaaeb2d34105414cf2e18b1fc6c97
Emanuel Dos Santos <emanueldossantoset5@gmail.com>
2021-12-12 23:39:20 -0300

    fix

--- database/migrations/2021_12_10_160505_create_company_table.php
+++ database/migrations/2021_12_10_160505_create_company_table.php
@@ -15,7 +15,7 @@ class CreateCompanyTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unqiue();
+            $table->string('name');
             $table->string('country');
         });
     }
